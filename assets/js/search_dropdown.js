9618881e6088220ed404b8dc9ac0f758a69c8d96
iisaiaaah <vincearomingurtiza@gmail.com>
2022-10-21 08:20:34 +0800

    dropdown update

---
+++ assets/js/search_dropdown.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    // $("body").on("click","#recruiter_search_dropdown", function(){
+    //     $("#recruiter_list").slideToggle();
+    //     // alert("urgh!")
+    // });
+
+    $(".search_dropdown select").selectpicker();
+}); 
