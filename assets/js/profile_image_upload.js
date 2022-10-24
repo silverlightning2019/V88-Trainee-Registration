e07df0d3e3e0f5fa1d63d007697c87b0855a0bfa
iisaiaaah <vincearomingurtiza@gmail.com>
2022-10-24 17:08:40 +0800

    Drag drop preview profile image

--- assets/js/profile_image_upload.js
+++ assets/js/profile_image_upload.js
@@ -5,12 +5,13 @@ $(document).ready(function(){
     
     function fasterPreview( uploader ) {
         if ( uploader.files && uploader.files[0] ){
-              $('#profile_image').attr('src', 
-                 window.URL.createObjectURL(uploader.files[0]) );
+            $('#profile_image').attr('src', 
+            window.URL.createObjectURL(uploader.files[0]) );
         }
     }
     
     $("#image_upload").change(function(){
         fasterPreview( this );
     });
+    UnoDropZone.init();
 })
