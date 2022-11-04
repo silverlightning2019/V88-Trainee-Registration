60f7c4f122056d038da3f80b91e6e269240b23b0
iisaiaaah <vincearomingurtiza@gmail.com>
2022-11-04 10:06:54 +0800

    Change id name on add trainee btn

--- assets/js/add_edit_modal.js
+++ assets/js/add_edit_modal.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
     $("body").on("click", "#edit_trainee_btn_modal", function(){
-        $("body").removeClass("#recruiter_block");
         $("#recruiter_block").hide();
         $("#status_block").show();
     });
-    $("body").on("click", "#add_trainee_btn_modal", function(){
+    $("body").on("click", "#trainee_btn_modal", function(){
         $("#recruiter_block").show();
         $("#status_block").hide();
     });
