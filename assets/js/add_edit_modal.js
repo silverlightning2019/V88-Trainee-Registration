19414e49987eeb4e7f93fb42ae298177142e3f1b
iisaiaaah <vincearomingurtiza@gmail.com>
2022-11-04 11:27:15 +0800

    Add & edit btn modal update

--- assets/js/add_edit_modal.js
+++ assets/js/add_edit_modal.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
-    $("body").on("click", "#edit_trainee_btn_modal", function(){
+    $("body").on("click", ".edit", function(){
         $(".filethumbnail").removeClass("font_size");
-        $(".filethumbnail").addClass("letter_profile");
         $(".filethumbnail").removeClass("upload_image_icon");
         $("#recruiter_block").hide();
         $("#status_block").show();
