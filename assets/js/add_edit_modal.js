d95ef43367e797b75ba50125191382d78cc5f2e7
iisaiaaah <vincearomingurtiza@gmail.com>
2022-11-04 09:28:16 +0800

    Add edit modal btn and upload background image

--- assets/js/add_edit_modal.js
+++ assets/js/add_edit_modal.js
@@ -1,13 +1,11 @@
 $(document).ready(function(){
     $("body").on("click", "#edit_trainee_btn_modal", function(){
-        alert("EDIIITTTTT!");
-        console.log("hi!");
-        // $("#recruiter_block").hide();
-        // $("#status_block").show();
+        $("body").removeClass("#recruiter_block");
+        $("#recruiter_block").hide();
+        $("#status_block").show();
     });
     $("body").on("click", "#add_trainee_btn_modal", function(){
-        alert("ADDDD!!!");
-        // $("#recruiter_block").hide();
-        // $("#status_block").show();
+        $("#recruiter_block").show();
+        $("#status_block").hide();
     });
 }); 
--- assets/js/search_dropdown.js
+++ assets/js/search_dropdown.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
-    // $("body").on("click","#recruiter_search_dropdown", function(){
-    //     $("#recruiter_list").slideToggle();
-    //     // alert("hi!")
-    // });
     $(function () {
         $('select').selectpicker();
     });
